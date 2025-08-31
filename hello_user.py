c1d773c3471a2b1f089ef1626cf024e418d4d486
Rachel Gladys <gladysrachel197@gmail.com>
2025-08-31 20:50:47 +0100

    Initial commit - hello_user

---
+++ hello_user.py
@@ -0,0 +1,21 @@
+#Hello User App
+# Step 1: Ask the user For their name
+name=input("what is your name?")
+# Step 2: Say hello
+print ("Hello " +  name + "! Nice to meet you!")
+# Step3: Ask user for their age
+age=int(input( "how old are you?"))
+# Step4: Do a small calculation 
+year_of_birth=2025-age
+print( "Cool! You were probably born in " + str(year_of_birth))
+future_age=age+10
+print( "In 10 years you will be  " + str(future_age) + " years old!")
+
+
+
+
+
+
+
+
+
